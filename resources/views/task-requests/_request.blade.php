d63bdf6d3bdcfdc3629a06408be95304952788cf
Pavel Koch <pavel.koch@wrongware.cz>
2016-10-14 13:06:51 +0100

    Check icon fix

--- resources/views/task-requests/_request.blade.php
+++ resources/views/task-requests/_request.blade.php
@@ -7,7 +7,7 @@
         <form action="task-requests/{{ $request->id }}/approve" method="post">
             {{ csrf_field() }}
 
-            <button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
+            <button class="mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect mdl-button--colored">
                 <i class="material-icons">check</i>
             </button>
         </form>
