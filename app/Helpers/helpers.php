c4bfd1b9c6cca9956840674d48451b4d2abc0de7
Pavel Koch <pavel.koch@wrongware.cz>
2016-11-02 16:36:22 +0000

    fixes

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -11,3 +11,20 @@ if (! function_exists('filter')) {
         return app('filter');
     }
 }
+
+if (! function_exists('socialite')) {
+    /**
+     * Socialite helper function
+     *
+     * @param  string  $provider
+     * @return \Laravel\Socialite\Contracts\Factory
+     */
+    function socialite($provider = null)
+    {
+        if (! $provider) {
+            return app('Laravel\Socialite\Contracts\Factory');
+        }
+
+        return app('Laravel\Socialite\Contracts\Factory')->driver($provider);
+    }
+}
--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\SocialAccount;
 use App\Http\Controllers\Controller;
-use Laravel\Socialite\Facades\Socialite;
 
 class SocialAuthController extends Controller
 {
@@ -16,7 +15,7 @@ class SocialAuthController extends Controller
      */
     public function redirect($provider)
     {
-        return Socialite::driver($provider)->redirect();
+        return socialite($provider)->redirect();
     }
 
     /**
@@ -27,7 +26,7 @@ class SocialAuthController extends Controller
      */
     public function callback($provider)
     {
-        $rawUser = Socialite::driver($provider)->user();
+        $rawUser = socialite($provider)->user();
 
         // We need to check whether the returned user already has this
         // type of social auth active. If yes, update it and return the
