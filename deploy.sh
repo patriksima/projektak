a81a36c4e33b4ef529a143267925354a5aa098e5
Pavel Koch <pavel.koch@wrongware.cz>
2016-11-09 14:34:29 +0000

    Deploy fix

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 rsync -r -e "ssh -p 2222 -o 'StrictHostKeyChecking no'" --delete-after $TRAVIS_BUILD_DIR/. vu3015@ares.wrongware.cz:root/subdomains/projektak
-ssh -p 2222 -o 'StrictHostKeyChecking no' vu3015@ares.wrongware.cz 'cd root/subdomains/projektak; composer install; php artisan migrate;'
+ssh -p 2222 -o 'StrictHostKeyChecking no' vu3015@ares.wrongware.cz 'cd root/subdomains/projektak; composer install; composer update; npm update; php artisan migrate:refresh --seed;'
